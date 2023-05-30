d66cd737fbdec3233a41241779e8892c38752ac8
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-05-30 03:06:35 -0400

    Test codigos tarea 1

---
+++ CompradorTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompradorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
---
+++ ExpendedorTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpendedorTest {
+
+    @Test
+    void comprarBebida() {
+    }
+
+    @Test
+    void getPrecio() {
+    }
+
+    @Test
+    void getVuelto() {
+    }
+}
