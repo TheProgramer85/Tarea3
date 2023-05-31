4cedd220ed854610c398428a48dbb6d933f26a23
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-05-31 01:53:24 -0400

    ExpendedorTest.java
    Avance test Expendedor, comprar producto testeado

--- ExpendedorTest.java
+++ ExpendedorTest.java
@@ -1,13 +1,28 @@
-package org.example;
+package org.example.expendedor;
 
+import org.example.Expendedor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExpendedorTest {
+    private Expendedor expendedor;
+    @BeforeEach
+    void setUp(){
+        expendedor = new Expendedor(4);
+    }
+    @AfterEach
+    void tearDown(){
+    }
 
     @Test
-    void comprarBebida() {
+    @DisplayName("Test Un Producto")
+    public void testcomprarProducto() throws Exception {
+        System.out.println("ComprarUnProducto");
+        assertNotNull(expendedor.comprarProducto(pago));
     }
 
     @Test
