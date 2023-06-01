32ff00fc9e37f2fd83aa427bdf4b551325389752
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-05-31 20:50:42 -0400

    ExpendedorTest.java
    Test de excepciones en expendedor agregados

--- ExpendedorTest.java
+++ ExpendedorTest.java
@@ -1,12 +1,16 @@
 package org.example.expendedor;
 
 import org.example.Expendedor;
-import org.junit.jupiter.api.Test;
+import org.example.PagoIncorrectoException;
+import org.example.PagoInsuficienteException;
+import org.example.NoHayProductoException;
+import org.example.NohayMonedaException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ExpendedorTest {
     private Expendedor expendedor;
@@ -21,15 +25,46 @@ class ExpendedorTest {
     @Test
     @DisplayName("Test Un Producto")
     public void testcomprarProducto() throws Exception {
+        int pago = 1500;
         System.out.println("ComprarUnProducto");
-        assertNotNull(expendedor.comprarProducto(pago));
+        expendedor.comprarProducto(pago);
     }
 
     @Test
-    void getPrecio() {
+    @DisplayName("Test No hay Moneda")
+    public void testNohayMonedaException() throws Exception{
+        int pago = 0;
+        Exception exception = assertThrows(NohayMonedaException.class()->){
+            expendedor.comprarProducto(pago);
+            expendedor.comprarProducto(pago);
+        }
     }
 
     @Test
-    void getVuelto() {
+    @DisplayName("test No hay Producto")
+    public void testNohayProductoException() throws Exception {
+        int pago = 1500;
+        Exception exception = assertThrows(NoHayProductoException.class,()->){
+            expendedor.comprarProducto(pago);
+        }
     }
+
+    @Test
+    @DisplayName("test Pago Incorrecto")
+    public void testPagoIncorrectoException() throws Exception{
+        int pago = 1;
+        Exception exception = assertThrows(PagoIncorrectoException.class,()->){
+            expendedor.comprarProducto(pago);
+        }
+    }
+
+    @Test
+    @DisplayName("test Pago Insuficiente")
+    public void testPagoInsuficienteException() throws Exception{
+        int pago = 50;
+        Exception exception = assertThrows(PagoInsuficienteException.class,()->){
+            expendedor.comprarProducto(pago);
+        }
+    }
+
 }
