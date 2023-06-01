0b429c8a886907db75d38e1cd53f2af95bc67449
Maticruces <maticruces2021@inf.udec.cl>
2023-05-31 22:13:59 -0400

    agregacion de test unitarios, con los metodos de analisis de las clase moneda y producto

--- MonedaTest.java
+++ MonedaTest.java
@@ -1,7 +1,28 @@
-package org.example;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MonedaTest {
+    @Test
+    void testGetValorMoneda1500() {
+        Moneda moneda = new Moneda1500();
+        assertEquals(1500, moneda.getValor());
+    }
 
+    @Test
+    void testGetValorMoneda1000() {
+        Moneda moneda = new Moneda1000();
+        assertEquals(1000, moneda.getValor());
+    }
+
+    @Test
+    void testGetValorMoneda500() {
+        Moneda moneda = new Moneda500();
+        assertEquals(500, moneda.getValor());
+    }
+
+    @Test
+    void testGetValorMoneda100() {
+        Moneda moneda = new Moneda100();
+        assertEquals(100, moneda.getValor());
+    }
 }
--- ProductoTest.java
+++ ProductoTest.java
@@ -1,16 +1,35 @@
-package org.example;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ProductoTest {
+public class ProductoTest {
 
     @Test
-    void getSerie() {
+    public void testConsumir() {
+        Producto sprite = new Sprite(123);
+        assertEquals("sprite", sprite.consumir());
+
+        Producto cocaCola = new CocaCola(456);
+        assertEquals("cocacola", cocaCola.consumir());
+
+        Producto super8 = new Super8(789);
+        assertEquals("super8", super8.consumir());
+
+        Producto rayita = new Rayita(999);
+        assertEquals("rayita", rayita.consumir());
     }
 
     @Test
-    void consumir() {
+    public void testGetSerie() {
+        Producto sprite = new Sprite(123);
+        assertEquals(123, sprite.getSerie());
+
+        Producto cocaCola = new CocaCola(456);
+        assertEquals(456, cocaCola.getSerie());
+
+        Producto super8 = new Super8(789);
+        assertEquals(789, super8.getSerie());
+
+        Producto rayita = new Rayita(999);
+        assertEquals(999, rayita.getSerie());
     }
 }
